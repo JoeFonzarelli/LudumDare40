590ffa112d982c358faee43a6590d0e12bd85081
Victor Freixes <vfreixes@gmail.com>
2017-12-02 19:36:54 +0100

    Shake in progress

--- LudumDare40/Assets/CameraShake.cs
+++ LudumDare40/Assets/CameraShake.cs
@@ -2,15 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraShake : MonoBehaviour {
+public class CameraShake : MonoBehaviour
+{
+	// Transform of the camera to shake. Grabs the gameObject's transform
+	// if null.
+	public Transform camTransform;
 
-	// Use this for initialization
-	void Start () {
-		
+	public bool isShaking = false;
+	// How long the object should shake for.
+	public float shakeDuration = 0f;
+
+	// Amplitude of the shake. A larger value shakes the camera harder.
+	public float shakeAmount = 0.7f;
+	public float decreaseFactor = 1.0f;
+
+	Vector3 originalPos;
+	float originalShakeTime;
+
+	void Awake()
+	{
+		if (camTransform == null)
+		{
+			camTransform = GetComponent(typeof(Transform)) as Transform;
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void OnEnable()
+	{
+		originalPos = camTransform.localPosition;
+		originalShakeTime = shakeDuration;
 	}
+
+	void Update()
+	{
+		if (isShaking) {
+			if (shakeDuration > 0) {
+				camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+
+				shakeDuration -= Time.deltaTime * decreaseFactor;
+			} else {
+				isShaking = false;
+				//shakeDuration = 0f;
+				camTransform.localPosition = originalPos;
+			}
+		}
+	}
+		
 }
